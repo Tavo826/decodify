2ed723a84b934bfb234e2d8977a5c8b59120e358
Gustavo Orozco Maya <Gustavo.Maya@segurossura.com.mx>
2024-08-28 12:06:32 -0500

    add vercel.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const CryptoJS = require("crypto-js")
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
